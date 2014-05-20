3af4ad314f46f04abe15b41a07c16a88162ea931
hollowGodric <godric@bckroll.com>
2014-05-20 11:47:50 +0200

    possible to stop and restart program

--- track.py
+++ track.py
@@ -1,14 +1,17 @@
 from expenseTracker import *
 
-while (True):
-	i = input("What do you want to do?\n")
-	args = i.split()
-	if (args[0] in commands):
-		commands[args[0]](args[1:])
-	elif i == 'stop':
-		break
-	else:
-		print("You may use the following commands: ")
-		for com in commands.keys():
-			print ("[%s] " % com, end="")
-		print("")
+def start():
+        while (True):
+                i = input("What do you want to do?\n")
+                args = i.split()
+                if (args[0] in commands):
+                        commands[args[0]](args[1:])
+                elif i == 'stop':
+                        break
+                else:
+                        print("You may use the following commands: ")
+                        for com in commands.keys():
+                                print ("[%s] " % com, end="")
+                        print("")
+
+start()
